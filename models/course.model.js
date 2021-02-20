517357246f5cfde70cf2eb8d2759a4d2b1846b9a
Hemant Jain <hmtj99@gmail.com>
2021-02-21 04:39:09 +0530

    fix: shift fields to courses

--- models/course.model.js
+++ models/course.model.js
@@ -4,7 +4,9 @@ const User = require('./user.model');
 const CourseSchema = new mongoose.Schema({
     instructor: {type: mongoose.Types.ObjectId, ref: 'User'},
     title: {type:String, required: true},
-    lectures: [{type: mongoose.Types.ObjectId, ref: 'Lecture'}]
+    lectures: [{type: mongoose.Types.ObjectId, ref: 'Lecture'}],
+    studentsEnrolled: [{type: mongoose.Types.ObjectId, ref:'Users'}],
+    studentsEligibleToEnroll: [{type: mongoose.Types.ObjectId, ref:'Users'}],
 },
 {
     collection:'courses',
--- models/lecture.model.js
+++ models/lecture.model.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
-const Course = require('./course.model');
 
 const LectureSchema = new mongoose.Schema({
     course: {type: mongoose.Types.ObjectId, ref: 'Course'},
     title: {type:String, required: true},
-    studentsEnrolled: [{type: mongoose.Types.ObjectId, ref:'Users'}],
-    studentsEligibleToEnroll: [{type: mongoose.Types.ObjectId, ref:'Users'}],
 },
 {
     collection:'lectures',
