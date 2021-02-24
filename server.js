b1e6b0a22a06e5c41b18174d4f609a8b44b3377e
Umesh <201801189@daiict.ac.in>
2021-02-24 12:20:56 +0530

    may put port number in keys

--- server.js
+++ server.js
@@ -28,7 +28,12 @@ app.get('/', (req,res) => {
     res.render('home');
 })
 
+//const PORT = process.env.PORT || 3000
 
+//app.listen(
+ // PORT,
+  //console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+//) 
 app.listen(3000, () => {
     console.log("Server has started!!!");
     mongoose.connect(keys.mongoURI,
