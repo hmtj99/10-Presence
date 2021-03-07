cd33e7a46bb8884939fda14e3c06d2cb1b12f350
Hemant Jain <hmtj99@gmail.com>
2021-03-08 02:33:01 +0530

    feat: sendAttendanceLink endpoint added

---
+++ modules/email.js
@@ -0,0 +1,116 @@
+const nodemailer = require('nodemailer');
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys');
+
+const NEW_PASSWORD_TOKEN_EXPIRY_DURATION = '5m';
+const BASE_URL = 'http://localhost:3000';
+const JWTSECRET = 'asdnalsdlashliahciiaslhclshclisahlijdlasjldjsaldbxzbcasdasdasdas';
+
+const generateTokenFromUserAndLecture = (userId,lectureId,courseId) => {
+    const secret = JWTSECRET;
+    const token = jwt.sign({userId,courseId,lectureId},secret,{
+        expiresIn: NEW_PASSWORD_TOKEN_EXPIRY_DURATION,
+    });
+    return token;
+}
+
+const getMarkAttendanceURL = (user,token) => {
+    return `${BASE_URL}/course/markAttendance?token=${token}&userId=${user._id}`;
+}
+
+const getEmailTemplate = (user,lecture,url) => {
+    const from = keys.EMAIL_LOGIN
+    const to = user.email
+    const subject = "Mark Attendance"
+    const html = `
+    <!doctype html>
+    <html lang="en-US">
+    <head>
+        <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+        <title>Mark Attendance</title>
+        <meta name="description" content="Mark Attendance Email Template.">
+        <style type="text/css">
+            a:hover {text-decoration: underline !important;}
+        </style>
+    </head>
+    <body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+        <!--100% body table-->
+        <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+            style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+            <tr>
+                <td>
+                    <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
+                        align="center" cellpadding="0" cellspacing="0">
+                        <tr>
+                            <td style="height:80px;">&nbsp;</td>
+                        </tr>
+                       
+                        <tr>
+                            <td style="height:20px;">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                                    style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                                    <tr>
+                                        <td style="height:40px;">&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="padding:0 35px;">
+                                            <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">Mark Your Attendance For This Lecture</h1>
+                                            <span
+                                                style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                            <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">   
+                                             Hey ${user.email.split('@')[0]},
+                                            </p>
+                                            <p style="color:#455056; font-size:15px;line-height:24px;margin-top:35px;">
+                                                Mark Your Attendance for the Lecture - ${lecture.title}                                          
+                                            </p>
+                                            <a href="${url}"
+                                                style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Mark Attendance</a>              
+                                            <p style="color:#455056; font-size:15px;line-height:24px; margin-top:35px;">
+                                                The link will only be active for 5 mins, Please mark your attendance before it.
+                                            </p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="height:40px;">&nbsp;</td>
+                                    </tr>
+                                </table>
+                            </td>
+                        <tr>
+                            <td style="height:20px;">&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td style="text-align:center;">
+                                <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>Powered by Presence, 2021</strong></p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="height:80px;">&nbsp;</td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+        <!--/100% body table-->
+    </body>
+    </html>`
+
+    return { from, to, subject, html }
+}
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: keys.EMAIL_LOGIN,
+      pass: keys.EMAIL_PASSWORD
+    }
+});
+
+module.exports = {
+    generateTokenFromUserAndLecture,
+    getEmailTemplate,
+    getMarkAttendanceURL,
+    transporter,
+}
--- routes/course-routes.js
+++ routes/course-routes.js
@@ -2,7 +2,15 @@ const router = require("express").Router();
 const Course = require('../models/course.model');
 const User = require('../models/user.model');
 const Lecture = require('../models/lecture.model');
-const mongoose = require("mongoose");
+const {
+    generateTokenFromUserAndLecture,
+    getEmailTemplate,
+    getMarkAttendanceURL,
+    transporter,
+} = require('../modules/email');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const secret = 'asdnalsdlashliahciiaslhclshclisahlijdlasjldjsaldbxzbcasdasdasdas';
 
 // Work in progress - Code for testing 
 router.get('/', async (req,res) => {
@@ -14,7 +22,9 @@ router.get('/', async (req,res) => {
         const doc = await Course.create({
             instructor: req.user.id,
             title: 'test course 1',
-            lectures: []
+            lectures: [],
+            studentsEnrolled: [mongoose.Types.ObjectId('60319538bc0bc9c95d46a91c'),mongoose.Types.ObjectId('601fff7b30add2b29e08c547')],
+            studentsEligibleToEnroll: [mongoose.Types.ObjectId('60319538bc0bc9c95d46a91c'),mongoose.Types.ObjectId('601fff7b30add2b29e08c547')],
         });
         const currentUser = await User.findById(req.user.id);
         currentUser.coursesTeaching.push(doc._id);
@@ -35,9 +45,7 @@ router.get('/lecture', async (req,res) => {
         const course = await Course.findOne();
         const doc = await Lecture.create({
             course: course._id,
-            title: 'test lecture 1',
-            studentsEnrolled: [],
-            studentEligibleToEnroll: [],
+            title: 'test lecture email 2',
         });
         course.lectures.push(doc._id);
         course.save();
@@ -47,6 +55,76 @@ router.get('/lecture', async (req,res) => {
         console.log(error);
     }
 })
+
+// http://localhost:3000/course/sendLectureAttendanceLinks?lectureId=6045362a668a248c19e2943b
+
+router.get('/sendLectureAttendanceLinks', async (req,res) => {
+    const lectureId = req.query.lectureId;
+    const lecture = await Lecture.findById(lectureId);
+    const course = await Course.findById(lecture.course);
+    const studentsEnrolled = course.studentsEnrolled;
+
+    for(const studentId of studentsEnrolled){
+        const student = await User.findById(studentId);
+        console.log(`Student - ${student._id}, Lecture - ${lecture._id}, Course - ${course._id}`);
+
+        const token = await generateTokenFromUserAndLecture(student._id,lecture._id,course._id);
+        const url = getMarkAttendanceURL(student,token);
+        const emailTemplate = getEmailTemplate(student,lecture,url);
+        await transporter.sendMail(emailTemplate, (err,info) => {
+            if(err){
+                console.log(err);
+                return res.json({status:'error',message:'Error while sending link to user'});
+            }
+            console.log(`Email sent to ${user.email}`);
+        })
+    }
+    return res.json({status:'ok',message:'All mails sent'});
+})
+
+router.get('/markAttendance', async (req,res) => {
+    const token = req.query.token;
+    const userId = req.query.userId;
+
+    let user
+    try{
+        user = await User.findById(userId).exec();
+    }
+    catch(error){
+        return res.json({status:'error',message:"User not found"});
+    }
+
+    let payload
+    try{
+        payload = await jwt.verify(token,secret);
+    }
+    catch(error){
+        return res.json({status:'error',message:"Link used/expired"});
+    }
+    console.log(user);
+    if(payload.userId === user._id.toString()){
+        try{
+            const lectureArray = user.courseLectureMap.get(payload.courseId);
+            if(lectureArray){
+                if(lectureArray.includes(mongoose.Types.ObjectId(payload.lectureId))){
+                    return res.json({status:'error', message: 'Attendance already marked'});
+                }
+                user.courseLectureMap.set(payload.courseId, [...lectureArray,payload.lectureId]);
+            }
+            else{
+                user.courseLectureMap.set(payload.courseId, [payload.lectureId]);
+            }
+            await user.save();
+        }
+        catch(error){
+            return res.json({status:'error',message:"Error marking attendance"});
+        }
+    }
+
+    res.send("<h1>Attendance Marked</h1>");
+
+})
+
 
 
 module.exports = router;
