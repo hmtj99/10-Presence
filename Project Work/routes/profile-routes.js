b8f0035e3ad6be5b15633fc3abee39d26906f20d
Keval <thakararkeval1010@gmail.com>
2021-03-31 11:44:12 +0530

    complete the get lecturelist function

--- Project Work/routes/profile-routes.js
+++ Project Work/routes/profile-routes.js
@@ -14,8 +14,8 @@ router.get('/', (req, res) => {
 })
 
 
-
 //http://localhost:3000/profile/getlecturelist?userId=60319538bc0bc9c95d46a91c
+//http://localhost:3000/profile/getlecturelist?userId=604d035ff6da592590f1c655
 
 router.get('/getlecturelist', async(req, res) => {
     if (!req.user) {
@@ -26,31 +26,58 @@ router.get('/getlecturelist', async(req, res) => {
         const user_ids = req.query.userId;
         const us = await User.findById(user_ids);
 
-        console.log(us.courseLectureMap);
+        //console.log(us.courseLectureMap);
+
+
         //res.send(us);
-        /* var course_attend = [];
+        let lecture_attend = [];
         us.courseLectureMap.forEach(item => {
-            console.log(item);
+            //console.log(item);
+            lecture_attend.push(item);
         });
 
-        */
+
+
+        // for (var i = 0; i < us.courseLectureMap)
+        //console.log(lecture_attend);
 
         Course.find({ studentsEnrolled: user_ids }, function(err, doc) {
 
             if (err) {
                 console.log(err);
             } else {
-                console.log(doc);
+                //console.log(doc);
 
                 doc.forEach(item => {
-                    console.log(item);
+                    // console.log(item);
+                    var numberof_lecture_per_course = item.lectures.length;
+                    var count_attend_lecture = 0;
+                    for (var i = 0; i < numberof_lecture_per_course; i++) {
+
+                        var ok = false;
+
+                        for (var j = 0; j < lecture_attend.length; j++) {
+
+                            //console.log(lecture_attend[j]);
+                            //console.log(item.lectures[i]);
+
+
+                            if (lecture_attend[j].includes(item.lectures[i])) {
+                                count_attend_lecture++;
+                                ok = true;
+                                break;
+                            }
+
+                        }
+
+                        console.log(i + " lecture : " + ok);
+                    }
+
+                    console.log("Course name  = " + item.title);
+                    console.log("attended lecture =  " + count_attend_lecture);
+                    console.log("total lecture = " + numberof_lecture_per_course);
                 });
-
-
-
-
             }
-
 
         });
 
