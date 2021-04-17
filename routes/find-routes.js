68e3988635b79571cffbef6fc6aba3574d9570b0
Umesh <201801189@daiict.ac.in>
2021-04-17 21:43:28 +0530

    add find lecture rout

--- routes/find-routes.js
+++ routes/find-routes.js
@@ -16,11 +16,25 @@ const authCheck = (req, res, next) => {
 router.get('/course',authCheck,(req,res)=>{
     res.render('findCourse');
 });
+
+router.get('/lecture',authCheck,(req,res)=>{
+    res.render('findLecture');
+});
+
 router.post('/course',(req,res)=>{
     Course.findOne({ _id: req.body.Cid }).lean().
     then(course => res.json({ course })).
     catch(error => res.json({ error: error.message }));
 
 });
+
+
+router.post('/lecture',(req,res)=>{
+    Lecture.findOne({ _id: req.body.Lid }).lean().
+    then(lecture => res.json({ lecture })).
+    catch(error => res.json({ error: error.message }));
+
+});
+
 
 module.exports = router;
