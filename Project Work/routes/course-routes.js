998143605180f8add3cff3a4d635f339e04f094e
Keval <thakararkeval1010@gmail.com>
2021-04-17 23:18:23 +0530

    endpoint for attendnce stats

--- Project Work/routes/course-routes.js
+++ Project Work/routes/course-routes.js
@@ -12,43 +12,42 @@ const {
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const secret = 'asdnalsdlashliahciiaslhclshclisahlijdlasjldjsaldbxzbcasdasdasdas';
-const csv=require('csvtojson');
+const csv = require('csvtojson');
 const { Router, json } = require("express");
 
 const fs = require('fs');
 const multer = require('multer');
-const upload = multer({dest: 'tmp/csv/'});
+const upload = multer({ dest: 'tmp/csv/' });
 
 // Work in progress - Code for testing 
-router.get('/', async (req,res) => {
-    if(!req.user){
+router.get('/', async(req, res) => {
+    if (!req.user) {
         res.redirect('/auth/login');
         return;
     }
-    try{
+    try {
         const doc = await Course.create({
             instructor: req.user.id,
             title: 'test course 1',
             lectures: [],
-            studentsEnrolled: [mongoose.Types.ObjectId('60319538bc0bc9c95d46a91c'),mongoose.Types.ObjectId('601fff7b30add2b29e08c547')],
-            studentsEligibleToEnroll: [mongoose.Types.ObjectId('60319538bc0bc9c95d46a91c'),mongoose.Types.ObjectId('601fff7b30add2b29e08c547')],
+            studentsEnrolled: [mongoose.Types.ObjectId('60319538bc0bc9c95d46a91c'), mongoose.Types.ObjectId('601fff7b30add2b29e08c547')],
+            studentsEligibleToEnroll: [mongoose.Types.ObjectId('60319538bc0bc9c95d46a91c'), mongoose.Types.ObjectId('601fff7b30add2b29e08c547')],
         });
         const currentUser = await User.findById(req.user.id);
         currentUser.coursesTeaching.push(doc._id);
         currentUser.save();
         res.send("Course created");
-    }
-    catch(error){
+    } catch (error) {
         console.log(error);
     }
 })
 
-router.get('/lecture', async (req,res) => {
-    if(!req.user){
+router.get('/lecture', async(req, res) => {
+    if (!req.user) {
         res.redirect('/auth/login');
         return;
     }
-    try{
+    try {
         const course = await Course.findOne();
         const doc = await Lecture.create({
             course: course._id,
@@ -57,74 +56,69 @@ router.get('/lecture', async (req,res) => {
         course.lectures.push(doc._id);
         course.save();
         res.send("Lecture created");
-    }
-    catch(error){
+    } catch (error) {
         console.log(error);
     }
 })
 
 // http://localhost:3000/course/sendLectureAttendanceLinks?lectureId=6045362a668a248c19e2943b
 
-router.get('/sendLectureAttendanceLinks', async (req,res) => {
+router.get('/sendLectureAttendanceLinks', async(req, res) => {
     const lectureId = req.query.lectureId;
     const lecture = await Lecture.findById(lectureId);
     const course = await Course.findById(lecture.course);
     const studentsEnrolled = course.studentsEnrolled;
 
-    for(const studentId of studentsEnrolled){
+    for (const studentId of studentsEnrolled) {
         const student = await User.findById(studentId);
         console.log(`Student - ${student._id}, Lecture - ${lecture._id}, Course - ${course._id}`);
 
-        const token = await generateTokenFromUserAndLecture(student._id,lecture._id,course._id);
-        const url = getMarkAttendanceURL(student,token);
-        const emailTemplate = getEmailTemplate(student,lecture,url);
-        await transporter.sendMail(emailTemplate, (err,info) => {
-            if(err){
+        const token = await generateTokenFromUserAndLecture(student._id, lecture._id, course._id);
+        const url = getMarkAttendanceURL(student, token);
+        const emailTemplate = getEmailTemplate(student, lecture, url);
+        await transporter.sendMail(emailTemplate, (err, info) => {
+            if (err) {
                 console.log(err);
-                return res.json({status:'error',message:'Error while sending link to user'});
+                return res.json({ status: 'error', message: 'Error while sending link to user' });
             }
             console.log(`Email sent to ${user.email}`);
         })
     }
-    return res.json({status:'ok',message:'All mails sent'});
+    return res.json({ status: 'ok', message: 'All mails sent' });
 })
 
-router.get('/markAttendance', async (req,res) => {
+router.get('/markAttendance', async(req, res) => {
     const token = req.query.token;
     const userId = req.query.userId;
 
     let user
-    try{
+    try {
         user = await User.findById(userId).exec();
-    }
-    catch(error){
-        return res.json({status:'error',message:"User not found"});
+    } catch (error) {
+        return res.json({ status: 'error', message: "User not found" });
     }
 
     let payload
-    try{
-        payload = await jwt.verify(token,secret);
-    }
-    catch(error){
-        return res.json({status:'error',message:"Link used/expired"});
+    try {
+        payload = await jwt.verify(token, secret);
+    } catch (error) {
+        return res.json({ status: 'error', message: "Link used/expired" });
     }
     console.log(user);
-    if(payload.userId === user._id.toString()){
-        try{
+    if (payload.userId === user._id.toString()) {
+        try {
             const lectureArray = user.courseLectureMap.get(payload.courseId);
-            if(lectureArray){
-                if(lectureArray.includes(mongoose.Types.ObjectId(payload.lectureId))){
-                    return res.json({status:'error', message: 'Attendance already marked'});
+            if (lectureArray) {
+                if (lectureArray.includes(mongoose.Types.ObjectId(payload.lectureId))) {
+                    return res.json({ status: 'error', message: 'Attendance already marked' });
                 }
-                user.courseLectureMap.set(payload.courseId, [...lectureArray,payload.lectureId]);
-            }
-            else{
+                user.courseLectureMap.set(payload.courseId, [...lectureArray, payload.lectureId]);
+            } else {
                 user.courseLectureMap.set(payload.courseId, [payload.lectureId]);
             }
             await user.save();
-        }
-        catch(error){
-            return res.json({status:'error',message:"Error marking attendance"});
+        } catch (error) {
+            return res.json({ status: 'error', message: "Error marking attendance" });
         }
     }
 
@@ -134,48 +128,46 @@ router.get('/markAttendance', async (req,res) => {
 
 
 
-router.get('/getAttendance',async (req, res)=>{
-     courseId= req.query.courseId;
-     let course ;
+router.get('/getAttendance', async(req, res) => {
+    courseId = req.query.courseId;
+    let course;
 
-     try{
-         course = await Course.findById(courseId);
-    
-     }
-     catch(error){
-      return res.json({status:"error", message:"Course not found"});
-     }
-     const data = [];
-     if(course.instructor.toString() === req.user._id.toString()){
-         const students = course.studentsEnrolled;
-         const totalLectures = course.lectures.length;
-         if(totalLectures === 0){
-             return res.json({status:"error", message:"No lectures have been taken in this course"});
-         }
-         for(const studentId of students){
-             student = await User.findById(studentId);
-             if(!student.courseLectureMap){
-                 continue;
-             }
-             const attendedLectures = student.courseLectureMap.get(course._id.toString()).length;
-             percentage = ((attendedLectures/totalLectures)*100).toFixed(2);
-             data.push({
-                 id: student.email.split("@")[0],
-                 name: student.name,
-                 attendance: percentage,
-             })
-         }
-         console.log(data);
-         
-     }
-     else{
-         return res.json({status:"error", message:"The data can only be accessed by the instructor"});
-     }
-     
-     converter.json2csv(data, (err,csv) => {
-        if(err){
+    try {
+        course = await Course.findById(courseId);
+
+    } catch (error) {
+        return res.json({ status: "error", message: "Course not found" });
+    }
+    const data = [];
+    if (course.instructor.toString() === req.user._id.toString()) {
+        const students = course.studentsEnrolled;
+        const totalLectures = course.lectures.length;
+        if (totalLectures === 0) {
+            return res.json({ status: "error", message: "No lectures have been taken in this course" });
+        }
+        for (const studentId of students) {
+            student = await User.findById(studentId);
+            if (!student.courseLectureMap) {
+                continue;
+            }
+            const attendedLectures = student.courseLectureMap.get(course._id.toString()).length;
+            percentage = ((attendedLectures / totalLectures) * 100).toFixed(2);
+            data.push({
+                id: student.email.split("@")[0],
+                name: student.name,
+                attendance: percentage,
+            })
+        }
+        console.log(data);
+
+    } else {
+        return res.json({ status: "error", message: "The data can only be accessed by the instructor" });
+    }
+
+    converter.json2csv(data, (err, csv) => {
+        if (err) {
             console.log(err);
-            return res.json({status:"error", message:"Error sending CSV"});
+            return res.json({ status: "error", message: "Error sending CSV" });
         }
         console.log(csv);
         res.setHeader('Content-Type', 'text/csv');
@@ -186,93 +178,93 @@ router.get('/getAttendance',async (req, res)=>{
 
 
 const authCheck = (req, res, next) => {
-    if(!req.user){
+    if (!req.user) {
         res.redirect('/auth/login');
     } else {
         next();
     }
 };
-router.get('/NewCourse',authCheck,(req,res) => {
+router.get('/NewCourse', authCheck, (req, res) => {
     res.render('NewCourse');
 })
 
-router.get('/AllCourse',authCheck,(req,res) => {
+router.get('/AllCourse', authCheck, (req, res) => {
     Course.find({ instructor: req.user._id }).sort({ createdAt: -1 })
-    .then(result => {
-      res.render('AllCourse', { Courses: result });
-    })
-    .catch(err => {
-      console.log(err);
-    });
+        .then(result => {
+            res.render('AllCourse', { Courses: result });
+        })
+        .catch(err => {
+            console.log(err);
+        });
 })
 
-router.get('/:id',authCheck, (req, res) => {
+router.get('/:id', authCheck, (req, res) => {
     const id = req.params.id;
     console.log(id);
     Course.findById(id)
-      .then(result => {
-        res.render('NewLecture',{Course : result});
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
+        .then(result => {
+            res.render('NewLecture', { Course: result });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
 
-router.post('/NewLecture',async (req,res)=>{
-  const array = await Course.findById(req.body.course);
-  console.log(array); 
-  if(array){
-      const newLec =await Lecture.create({
-      course:array._id,
-      title: req.body.Ltitle,
-      start_of_lecture: req.body.start_of_lecture,
-      end_of_lecture: req.body.end_of_lecture,
-    });
-    array.lectures.push(newLec._id);
+router.post('/NewLecture', async(req, res) => {
+    const array = await Course.findById(req.body.course);
     console.log(array);
-    array.save();
-  
-  }
-  res.send("all done");
-});
+    if (array) {
+        const newLec = await Lecture.create({
+            course: array._id,
+            title: req.body.Ltitle,
+            start_of_lecture: req.body.start_of_lecture,
+            end_of_lecture: req.body.end_of_lecture,
+        });
+        array.lectures.push(newLec._id);
+        console.log(array);
+        array.save();
 
-router.post('/',upload.single('EligibleStudent'),async (req,res) =>{
-const array =await csv().fromFile(req.file.path);
-console.log(array);
-let Es=[];
-for(const n of array){
-    console.log(n.Gmail);
-    const user =await User.findOne({email : n.Gmail}).exec();
-    if(user){
-        Es.push(user._id);
     }
-}
-console.log(Es);
-console.log("this es",Es);
-const newCourse =await Course.create({
-    instructor: req.user.id,
-    title: req.body.Ctitle,
-    start_of_class: req.body.start_of_class,
-    end_of_class: req.body.end_of_class,
-    lectures: [],
-    studentsEnrolled: [],
-    studentsEligibleToEnroll:Es,
-    criteria:req.body.criteria,
+    res.send("all done");
 });
-fs.unlinkSync(req.file.path);
-res.send("done");
+
+router.post('/', upload.single('EligibleStudent'), async(req, res) => {
+    const array = await csv().fromFile(req.file.path);
+    console.log(array);
+    let Es = [];
+    for (const n of array) {
+        console.log(n.Gmail);
+        const user = await User.findOne({ email: n.Gmail }).exec();
+        if (user) {
+            Es.push(user._id);
+        }
+    }
+    console.log(Es);
+    console.log("this es", Es);
+    const newCourse = await Course.create({
+        instructor: req.user.id,
+        title: req.body.Ctitle,
+        start_of_class: req.body.start_of_class,
+        end_of_class: req.body.end_of_class,
+        lectures: [],
+        studentsEnrolled: [],
+        studentsEligibleToEnroll: Es,
+        criteria: req.body.criteria,
+    });
+    fs.unlinkSync(req.file.path);
+    res.send("done");
 });
 
 
-router.get('/findCourse',authCheck,(req,res)=>{
+router.get('/findCourse', authCheck, (req, res) => {
     res.render('findCourse');
 });
 
-router.get('/findLecture',authCheck,(req,res)=>{
+router.get('/findLecture', authCheck, (req, res) => {
     res.render('findLecture');
 });
 
-router.post('/findCourse',(req,res)=>{
+router.post('/findCourse', (req, res) => {
     Course.findOne({ _id: req.body.Cid }).lean().
     then(course => res.json({ course })).
     catch(error => res.json({ error: error.message }));
@@ -280,12 +272,32 @@ router.post('/findCourse',(req,res)=>{
 });
 
 
-router.post('/findLecture',(req,res)=>{
+router.post('/findLecture', (req, res) => {
     Lecture.findOne({ _id: req.body.Lid }).lean().
     then(lecture => res.json({ lecture })).
     catch(error => res.json({ error: error.message }));
 
 });
+
+
+
+// http://localhost:3000/course/getattendancecriteria?courseId=60450eaa7ce02484799abc5f
+
+router.get('/getattendancecriteria', async(req, res) => {
+    if (!req.user) {
+        res.redirect('/auth/login');
+        return;
+    }
+    try {
+
+        const cid = req.query.courseId;
+        const course_detail = await Course.findById(cid);
+        res.send('Attendance Criteria = ' + course_detail.criteria);
+
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 
 module.exports = router;
--- Project Work/routes/profile-routes.js
+++ Project Work/routes/profile-routes.js
@@ -1,11 +1,84 @@
 const router = require("express").Router();
 
-router.get('/', (req,res) => {
-    if(!req.user){
+router.get('/', (req, res) => {
+    if (!req.user) {
         res.redirect('/auth/login');
         return;
     }
-    res.render('profile',{user:req.user})
+    res.render('profile', { user: req.user })
+})
+
+
+//http://localhost:3000/profile/getlecturelist?userId=60319538bc0bc9c95d46a91c
+//http://localhost:3000/profile/getlecturelist?userId=604d035ff6da592590f1c655
+
+router.get('/getlecturelist', async(req, res) => {
+    if (!req.user) {
+        res.redirect('/auth/login');
+        return;
+    }
+    try {
+        const user_ids = req.query.userId;
+        const us = await User.findById(user_ids);
+
+        //console.log(us.courseLectureMap);
+
+
+        //res.send(us);
+        let lecture_attend = [];
+        us.courseLectureMap.forEach(item => {
+            //console.log(item);
+            lecture_attend.push(item);
+        });
+
+
+
+        // for (var i = 0; i < us.courseLectureMap)
+        //console.log(lecture_attend);
+
+        Course.find({ studentsEnrolled: user_ids }, function(err, doc) {
+
+            if (err) {
+                console.log(err);
+            } else {
+                //console.log(doc);
+
+                doc.forEach(item => {
+                    // console.log(item);
+                    var numberof_lecture_per_course = item.lectures.length;
+                    var count_attend_lecture = 0;
+                    for (var i = 0; i < numberof_lecture_per_course; i++) {
+
+                        var ok = false;
+
+                        for (var j = 0; j < lecture_attend.length; j++) {
+
+                            //console.log(lecture_attend[j]);
+                            //console.log(item.lectures[i]);
+
+
+                            if (lecture_attend[j].includes(item.lectures[i])) {
+                                count_attend_lecture++;
+                                ok = true;
+                                break;
+                            }
+
+                        }
+
+                        console.log(i + " lecture : " + ok);
+                    }
+
+                    console.log("Course name  = " + item.title);
+                    console.log("attended lecture =  " + count_attend_lecture);
+                    console.log("total lecture = " + numberof_lecture_per_course);
+                });
+            }
+
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
 })
 
 module.exports = router;
